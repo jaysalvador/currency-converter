2cf264636425da485992a63db57afc91eff5c598
Jay Andrae Salvador <jay.andrae@playsport.com>
2020-03-11 08:44:50 +1100

    Initial project commit

---
+++ Westpac/AppDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  Westpac
+//
+//  Created by Jay Salvador on 11/3/20.
+//  Copyright © 2020 Jay Salvador. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK: - Properties
+    
+    var window: UIWindow?
+    
+    class var shared: AppDelegate? {
+        
+        return UIApplication.shared.delegate as? AppDelegate
+    }
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // MARK: - UI
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        self.window?.rootViewController = ViewController()
+        
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
---
+++ Westpac/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Westpac
+//
+//  Created by Jay Salvador on 11/3/20.
+//  Copyright © 2020 Jay Salvador. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
